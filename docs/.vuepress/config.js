c5aac08a4a1e3881e59e51bbb3fcaddf05053731
Joker <1980015147@qq.com>
2023-03-23 22:16:19 +0800

    'update'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,21 +14,27 @@ module.exports = {
             {
                 title: '欢迎学习',
                 path: '/',
-                collapsable: true, // 是否折叠
+                collapsable: flase, // 是否折叠
                 children: [
                     { title: "博客介绍", path: "/" }
                 ]
             },
             {
-              title: "基础学习",
-              path: '/handbook/1',
-              title:"内容",
+            //   title: "基础学习",
+            //   path: '/handbook/1',
+              title:"Python基础",
               path: '/handbook/1',
               collapsable: true, // 是否折叠
               children: [
-                { title: "Python笔记", path: "/handbook/1" },    
-                { title: "CTF:攻防世界", path: "/handbook/2" }
-                // { title: "博客搭建", path: "/handbook/3" }
+                { title: "Python笔记", path: "/handbook/1" }    
+                // { title: "CTF:攻防世界", path: "/handbook/2" } 
+              ],
+              title:"CTF Wp:",
+              path: '/handbook/2',
+              collapsable: true, // 是否折叠
+              children: [
+                { title: "CTF:攻防世界", path: "/handbook/2" }    
+                // { title: "CTF:攻防世界", path: "/handbook/2" } 
               ],
             }
           ]
